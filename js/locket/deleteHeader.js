4f1e3cd9db485fc2790d63ec9db39ad010afab08
dhungx <139793488+dhungx@users.noreply.github.com>
2024-10-02 18:30:17 +0700

    Create deleteHeader.js

---
+++ js/locket/deleteHeader.js
@@ -0,0 +1,5 @@
+//ViBoss Studio  02/09/2024
+const version = 'V1.0.2';
+
+
+function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}var modifiedHeaders=$request.headers;setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag",""),$done({headers:modifiedHeaders});
