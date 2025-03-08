7c3a43b733da2cb9124577c039a023b6fae163f9
Đỗ Hùng <139793488+dhungx@users.noreply.github.com>
2025-03-08 15:38:45 +0700

    Update deleteHeader.js

--- js/locket/deleteHeader.js
+++ js/locket/deleteHeader.js
@@ -1,5 +1,34 @@
-//ViBoss Studio  02/09/2024
-const version = 'V1.0.2';
+// ViBoss Studio - 02/09/2024
+const version = 'V1.0.3';
 
+/**
+ * Hàm đặt giá trị cho header (tự động xử lý chữ hoa/thường)
+ * @param {object} headers - Đối tượng chứa header của request
+ * @param {string} headerName - Tên header cần chỉnh sửa
+ * @param {string} value - Giá trị mới cần gán cho header
+ */
+function setHeaderValue(headers, headerName, value) {
+    const lowerCaseHeader = headerName.toLowerCase();
+    if (lowerCaseHeader in headers) {
+        headers[lowerCaseHeader] = value;
+    } else {
+        headers[headerName] = value;
+    }
+}
 
-function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}var modifiedHeaders=$request.headers;setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag",""),$done({headers:modifiedHeaders});
+// Lấy danh sách header từ request
+let modifiedHeaders = { ...$request.headers };
+
+// Danh sách các header cần chỉnh sửa
+const headersToModify = {
+    "X-RevenueCat-ETag": "",      // Xóa giá trị ETag (tránh cache)
+    "X-Custom-Debug": "Enabled"   // (Tùy chọn) Thêm header debug
+};
+
+// Áp dụng các chỉnh sửa header
+for (const [key, value] of Object.entries(headersToModify)) {
+    setHeaderValue(modifiedHeaders, key, value);
+}
+
+// Hoàn tất request với header đã chỉnh sửa
+$done({ headers: modifiedHeaders });
