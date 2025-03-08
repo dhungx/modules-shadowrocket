c3844a380897dabdc1cc7a0ded123bed74a7546b
Đỗ Hùng <139793488+dhungx@users.noreply.github.com>
2025-03-08 17:05:34 +0700

    Update Locket.js

--- js/locket/Locket.js
+++ js/locket/Locket.js
@@ -1,65 +1,6 @@
-// ========= ID Mapping ========= //
+// ========= ID ========= //
 const mapping = {
-    '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
-    'Locket': ['Gold']
+  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
+  'Locket': ['Gold']
 };
-
-// ========= Xử lý User-Agent ========= //
-var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
-var obj;
-
-try {
-    obj = JSON.parse($response.body);
-} catch (error) {
-    console.log("🚨 Lỗi khi phân tích JSON:", error);
-    return $done({});
-}
-
-// ========= Thêm Thông Báo ========= //
-obj.Attention = "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
-
-// ========= Dữ Liệu Gói Đăng Ký Giả ========= //
-const fakeSubscription = {
-    is_sandbox: false,
-    ownership_type: "PURCHASED",
-    billing_issues_detected_at: null,
-    period_type: "normal",
-    expires_date: "2099-12-18T01:04:17Z",
-    grace_period_expires_date: null,
-    unsubscribe_detected_at: null,
-    original_purchase_date: "2024-07-28T01:04:18Z",
-    purchase_date: "2024-07-28T01:04:17Z",
-    store: "app_store"
-};
-
-const fakeEntitlement = {
-    grace_period_expires_date: null,
-    purchase_date: "2024-07-28T01:04:17Z",
-    expires_date: "2099-12-18T01:04:17Z"
-};
-
-// ========= Xác Định Ứng Dụng & Gán Gói ========= //
-const matchedApp = Object.keys(mapping).find(key => ua.includes(key));
-
-if (matchedApp) {
-    let [entitlementKey, productIdentifier] = mapping[matchedApp];
-
-    if (productIdentifier) {
-        fakeEntitlement.product_identifier = productIdentifier;
-        obj.subscriber.subscriptions[productIdentifier] = fakeSubscription;
-    } else {
-        obj.subscriber.subscriptions["com.ohoang7.premium.yearly"] = fakeSubscription;
-    }
-
-    obj.subscriber.entitlements[entitlementKey] = fakeEntitlement;
-} else {
-    // Mặc định nếu không tìm thấy ứng dụng
-    obj.subscriber.subscriptions["com.ohoang7.premium.yearly"] = fakeSubscription;
-    obj.subscriber.entitlements.pro = fakeEntitlement;
-}
-
-// ========= Log Debug ========= //
-console.log("🛠 Đã cập nhật đăng ký cho:", ua);
-
-// ========= Trả Về Dữ Liệu Đã Chỉnh Sửa ========= //
-$done({ body: JSON.stringify(obj) });
+var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],obj=JSON.parse($response.body);obj.Attention="Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";var ohoang7={is_sandbox:!1,ownership_type:"PURCHASED",billing_issues_detected_at:null,period_type:"normal",expires_date:"2099-12-18T01:04:17Z",grace_period_expires_date:null,unsubscribe_detected_at:null,original_purchase_date:"2024-07-28T01:04:18Z",purchase_date:"2024-07-28T01:04:17Z",store:"app_store"},vuong2023={grace_period_expires_date:null,purchase_date:"2024-07-28T01:04:17Z",product_identifier:"com.ohoang7.premium.yearly",expires_date:"2099-12-18T01:04:17Z"};const match=Object.keys(mapping).find(e=>ua.includes(e));if(match){let[e,s]=mapping[match];s?(vuong2023.product_identifier=s,obj.subscriber.subscriptions[s]=ohoang7):obj.subscriber.subscriptions["com.ohoang7.premium.yearly"]=ohoang7,obj.subscriber.entitlements[e]=vuong2023}else obj.subscriber.subscriptions["com.ohoang7.premium.yearly"]=ohoang7,obj.subscriber.entitlements.pro=vuong2023;$done({body:JSON.stringify(obj)});
