5ca211d754df20728a8408a7d6949bbb976d32b2
Đỗ Hùng <139793488+dhungx@users.noreply.github.com>
2025-03-08 16:50:24 +0700

    Update Locket.js

--- js/locket/Locket.js
+++ js/locket/Locket.js
@@ -27,14 +27,14 @@ const fakeSubscription = {
     expires_date: "2099-12-18T01:04:17Z",
     grace_period_expires_date: null,
     unsubscribe_detected_at: null,
-    original_purchase_date: "2025-03-08T01:04:18Z",
-    purchase_date: "2025-03-08T01:04:17Z",
+    original_purchase_date: "2024-07-28T01:04:18Z",
+    purchase_date: "2024-07-28T01:04:17Z",
     store: "app_store"
 };
 
 const fakeEntitlement = {
     grace_period_expires_date: null,
-    purchase_date: "2025-03-08T01:04:17Z",
+    purchase_date: "2024-07-28T01:04:17Z",
     expires_date: "2099-12-18T01:04:17Z"
 };
 
